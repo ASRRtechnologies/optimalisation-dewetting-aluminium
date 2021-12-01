029a1f05651cbffc28b5986e5253768601ada68d
cboonstra <lotte.boonstra@asrr.nl>
2021-12-01 16:18:14 +0100

    added path to image to script

--- Test.py
+++ Test.py
@@ -9,3 +9,8 @@ class Test:
         print(i)
 
     print("Hello world!")
+
+#import path
+path = /Users/lotteboonstra/projects/tud/optimalisation-dewetting-aluminium/images/test-images/Alu_SiC_K_2000.bmp
+
+
