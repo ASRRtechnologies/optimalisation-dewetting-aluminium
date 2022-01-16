29377ffc9e5541f672db3387b462e5f4750a8887
cboonstra <lotte.boonstra@asrr.nl>
2022-01-16 13:41:05 +0100

    Get working contour for alu test 1

--- AnalyseImage.py
+++ AnalyseImage.py
@@ -1,4 +1,5 @@
 import cv2
+import cv2.cv2
 import numpy as np
 import glob
 import csv
@@ -16,13 +17,13 @@ def get_contours(img, imgContour):
 
     # for each contour found
     for cnt in contours:
-        # cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 2)
+        cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 2)
         # find its area in pixel
         area = cv2.contourArea(cnt)
         print("Detected Contour with Area: ", area)
 
         # minimum area value is to be fixed as the one that leaves the coin as the small object on the scene
-        if (area > 5000):
+        if area > 5000:
             perimeter = cv2.arcLength(cnt, True)
 
             # smaller epsilon -> more vertices detected [= more precision]
@@ -41,7 +42,7 @@ def get_contours(img, imgContour):
 
     # drawing contours for the final objects
     for con in final_contours:
-        cv2.drawContours(imgContour, con[3], -1, (0, 0, 255), 3)
+        cv2.drawContours(imgContour, con[3], -1, (0, 0, 255), 3)  # color of line contour
 
     return imgContour, final_contours
 
@@ -49,46 +50,51 @@ def get_contours(img, imgContour):
 def process_image(path):
     # sourcing the input image
     img = cv2.imread(path)
-    cv2.imshow("Starting image", img)
+    # cv2.imshow("Starting image", img)
     # cv2.waitKey()
 
     # blurring
-    imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
+    imgBlur = cv2.GaussianBlur(img, (1, 1), 0)
+    # cv2.imshow("blury image", imgBlur)1
+    # cv2.waitKey()
+
     # graying
     imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-    # canny
-    imgCanny = cv2.Canny(imgGray, 100, 60)  # TODO: figure out correct upper and lower bounds for canny
 
-    kernel = np.ones((2, 2))
-    imgDil = cv2.dilate(imgCanny, kernel, iterations=3)
-    imgThre = cv2.erode(imgDil, kernel, iterations=3)
-    imgFinalContours, finalContours = get_contours(imgThre, img)
+    # Apply threshold - all values below 50 goes to 0, and values above 50 goes to 1.
+    ret, thresh_gray = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY)
+    # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
+    edges = cv2.Canny(thresh_gray, 50, 150, apertureSize=7)
 
-    # show  the contours on the unfiltered starting image
-    cv2.imshow("Final External Contours", imgFinalContours)
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
-    return len(finalContours)
+    minLineLength = 100
+    maxLineGap = 5
+    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=100, minLineLength=minLineLength,
+                            maxLineGap=maxLineGap)
 
-
-# function to determine the scale of the image
-
-def determine_scale(path):
-    # sourcing the input image
-    img = cv2.imread(path)
-    cv2.imshow("Starting image", img)
+    cv2.imshow("canny image", edges)
     cv2.waitKey()
 
-    # blurring
-    imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
-    # graying
-    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+
+    # # Draw lines
+    # for line in lines:
+    #     x1, y1, x2, y2 = line[0].tolist()
+    #     cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    #
+    # cv2.imwrite('houghlines.png', img)
+
     # canny
-    imgCanny = cv2.Canny(imgGray, 100, 60)  # TODO: figure out correct upper and lower bounds for canny
+    # imgCanny = cv2.Canny(imgGray, 150, 50)  # TODO: figure out correct upper and lower bounds for canny
+    # cv2.imshow("canny image", imgCanny)
+    # cv2.waitKey()
+
 
     kernel = np.ones((2, 2))
-    imgDil = cv2.dilate(imgCanny, kernel, iterations=3)
+    imgDil = cv2.dilate(edges, kernel, iterations=3)
+    # cv2.imshow("dilate", imgDil)
+    # cv2.waitKey()
     imgThre = cv2.erode(imgDil, kernel, iterations=3)
+    cv2.imshow("erode", imgThre)
+    cv2.waitKey()
     imgFinalContours, finalContours = get_contours(imgThre, img)
 
     # show  the contours on the unfiltered starting image
@@ -97,36 +103,63 @@ def determine_scale(path):
     cv2.destroyAllWindows()
     return len(finalContours)
 
+
+# function to determine the scale of the image
+
+# def determine_scale(path):
+#     # sourcing the input image
+#     img = cv2.imread(path)
+#     cv2.imshow("Starting image", img)
+#     cv2.waitKey()
+#
+#     # blurring
+#     imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
+#     # graying
+#     imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+#     # canny
+#     imgCanny = cv2.Canny(imgGray, 100, 60)  # TODO: figure out correct upper and lower bounds for canny
+#
+#     kernel = np.ones((2, 2))
+#     imgDil = cv2.dilate(imgCanny, kernel, iterations=3)
+#     imgThre = cv2.erode(imgDil, kernel, iterations=3)
+#     imgFinalContours, finalContours = get_contours(imgThre, img)
+#
+#     # show  the contours on the unfiltered starting image
+#     cv2.imshow("Final External Contours", imgFinalContours)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+#     return len(finalContours)
+
 # definition to determine the canny
-def auto_canny(image, sigma=0.33):
-    # compute the median of the single channel pixel intensities
-    v = np.median(image)
-    # apply automatic Canny edge detection using the computed median
-    lower = int(max(0, (1.0 - sigma) * v))
-    upper = int(min(255, (1.0 + sigma) * v))
-    edged = cv2.Canny(image, lower, upper)
-    # return the edged image
-    return edged
+# def auto_canny(image, sigma=0.33):
+#     # compute the median of the single channel pixel intensities
+#     v = np.median(image)
+#     # apply automatic Canny edge detection using the computed median
+#     lower = int(max(0, (1.0 - sigma) * v))
+#     upper = int(min(255, (1.0 + sigma) * v))
+#     edged = cv2.Canny(image, lower, upper)
+#     # return the edged image
+#     return edged
 
 
 class AnalyseImage:
-    paths = glob.glob("images/test-images/*.bmp")
+    paths = glob.glob("images/test-images/*.tif")
     print(paths)
 
     # determine scale of an image
-    determine_scale("images/test-images/10-times-5um-by-10-times-5-um.bmp")
+    # determine_scale("images/test-images/10-times-5um-by-10-times-5-um.bmp")
 
-    # # open the file in the write mode
-    # f = open(f'output/{datetime.now()}-output.csv', 'w+')
-    #
-    # # create the csv writer
-    # writer = csv.writer(f)
-    # writer.writerow(["PATH", "TOTAL FINAL CONTOURS"])
-    #
-    # for path in paths:
-    #     totalContours = process_image(path)
-    #     # write a row to the csv file
-    #     writer.writerow([path, totalContours])
-    #
-    # # close the file
-    # f.close()
+    # open the file in the write mode
+    f = open(f'output/{datetime.now()}-output.csv', 'w+')
+
+    # create the csv writer
+    writer = csv.writer(f)
+    writer.writerow(["PATH", "TOTAL FINAL CONTOURS"])
+
+    for path in paths:
+        totalContours = process_image(path)
+        # write a row to the csv file
+        writer.writerow([path, totalContours])
+
+    # close the file
+    f.close()
