7b69b7dc6ff07bf92f6cea66677f00a4b71656a2
cboonstra <lotte.boonstra@asrr.nl>
2022-02-04 01:50:05 +0100

    Finish script for now

--- ImageCleanup.py
+++ ImageCleanup.py
@@ -5,7 +5,7 @@ import os
 
 class ImageCleanup:
     print("Initializing helper")
-    paths = glob.glob("images/input/*")
+    paths = glob.glob("images/sanitized/*")
     print(len(paths))
 
     for path in paths:
@@ -17,16 +17,19 @@ class ImageCleanup:
         desiredName = desiredName.replace("175C", "175")
 
         # 120 batch
-        desiredName = desiredName.replace("Si-TiP_Alu-11", "Si_TiP-Alu")
+        desiredName = desiredName.replace("Si-TiP_Alu_11", "Si_TiP-Alu")
         desiredName = desiredName.replace("SiC-TiP_Alu-13", "SiC_TiP-Alu")
+        desiredName = desiredName.replace("SiC-TiP_Alu_12", "SiC_TiP-Alu")
         desiredName = desiredName.replace("SiN-TiP_Alu-14", "SiN_TiP-Alu")
         desiredName = desiredName.replace("Si-NbTiN_TiP_Alu-12", "Si-NbT_TiP-Alu")
+        desiredName = desiredName.replace("Si_NbTiN-TiP_Alu_12", "Si-NbT_TiP-Alu")
 
         #150 batch
         desiredName = desiredName.replace("Si-NbTiN_TiP_Alu-14", "Si-NbT_TiP-Alu")
         desiredName = desiredName.replace("Si-TiP_Alu-14", "Si_TiP-Alu")
         desiredName = desiredName.replace("SiC-TiP_Alu-14", "SiC_TiP-Alu")
         desiredName = desiredName.replace("SiN-TiP_Alu-14", "SiN_TiP-Alu")
+        desiredName = desiredName.replace("NbT-TiP_Alu-14", "Si-NbT_TiP-Alu")
 
         #170 batch
         desiredName = desiredName.replace("NbTiN-TiP_Alu-12", "Si-NbT_TiP-Alu")
@@ -40,5 +43,7 @@ class ImageCleanup:
         desiredName = desiredName.replace("SiC-TiP_Alu-12", "SiC_TiP-Alu")
         desiredName = desiredName.replace("SiN-TiP_Alu-12", "SiN_TiP-Alu")
 
-        print("Renaming ", path, " to ", desiredName)
+        if os.path.basename(path) != desiredName:
+            print("Renaming ", os.path.basename(path), " to ", desiredName)
+
         os.rename(path, "images/sanitized/" + desiredName)
